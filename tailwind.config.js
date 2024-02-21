f64bdcd20ec74afc86c105d2780e9a5014c32d8b
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-02-21 22:48:51 +0200

    Add Dave Gray YT tutorial 02_build-website header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // content: ["./src/**/*.{html,js}"],
-  content: ["./tutorials/**/*.{html,js}"],
+  content: [
+		"./tutorials/**/*.{html,js}",
+		"!./tutorials/**/_*.html"
+	],
   theme: {
     extend: {},
   },
